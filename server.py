dd2d76bb03ed0e78e5f83064ce75a53d070ad19a
Mridul Yadav <mriduly2@gmail.com>
2025-07-27 00:15:04 +0530

    Updated server.py

--- server.py
+++ server.py
@@ -37,5 +37,8 @@ def predict():
         print("❌ ERROR:", str(e))  # Print error in terminal
         return jsonify({"error": str(e)}), 500
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
